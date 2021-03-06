aa5fdd239ed11e92584bbfd75ec11742d29130d4
oadeniran <60529828+oadeniran@users.noreply.github.com>
2021-03-06 19:32:44 +0100

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,47 @@
+<?php
+
+	if (isset($_POST['mail']))
+	{
+		$email = $_POST['mail'];
+		$first = $_POST['FIRST'];
+		$base_password = $_POST['password'];
+		$conf_password = $_POST['conf_password'];
+		
+		if($base_password == $conf_password)
+		{		
+			$cookie_name = "USERNAME";
+			$cookie_2 = "PASSWORD";
+			setcookie($cookie_name,$_POST['username'],time() + 2400 *2);
+			setcookie($cookie_2,$_POST['password'], time() + 2400 *2);
+			include 'index.html';
+		}
+		else
+		{
+			echo("Passwords don't match");
+		}
+	}
+	elseif (isset($_POST['Login']))
+	{
+		if (!isset($_COOKIE['USERNAME']))
+		{
+			echo 'No Recent user found with Username in this session';
+			echo '<html><p><a href= "index.html">LOGIN</a></p> <p><a href="reg.html">REGISTER</a></p> </html>';
+		}
+		if ($_POST['username'] == $_COOKIE['USERNAME'])
+		{
+			if ($_POST['password'] == $_COOKIE['PASSWORD'])
+			{
+				echo('<h1> Login successful</h2>');
+			}
+			else{
+				echo 'password is Incorrect';
+				echo '<html><p><a href= "index.html">BACK</a></p> </html>';
+			}
+		}
+		else{
+			include ('index.html');
+                        echo 'No user found with Username';
+			echo '<html><p><a href= "index.html">LOGIN</a></p> <p><a href="reg.html">REGISTER</a></p> </html>';
+		}
+	}
+?>
